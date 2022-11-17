dc62405058a2c2ebc501e0734a9e81a2223205f2
MaksPunch <maks.punch@yandex.ru>
2022-11-17 10:48:34 +0300

    json file & post req finalized

--- app.js
+++ app.js
@@ -4,12 +4,30 @@ const app = express()
 const path = require("path");
 const cors = require("cors");
 const morgan = require("morgan");
+const jsonfile = require("jsonfile")
 var bodyParser = require('body-parser')
-const {variables, tasksData, priorityColor} = require(path.join(__dirname, "db/variables.js"));
-const dashboard = variables();
 const urlencodedParser = bodyParser.urlencoded({
   extended: false,
 })
+
+const filePath = "./db/tasks.json"
+
+let file = jsonfile.readFileSync(filePath)
+
+const variables = () =>  {
+  return {
+    title: 'TODO list',
+    tasksList: file,
+    openTasks: file.filter((a)=>(a.openStatus==true)),
+    closedTasks: file.filter((a)=>a.openStatus==false)
+  }
+}
+
+const priorityColor = (prio) => {
+  if (prio == 'Высокий приоритет') return 'red'
+  else if (prio == 'Низкий приоритет') return 'green'
+  else if (prio == 'Средний приоритет') return 'yellow'
+} 
 
 app.use(cors());
 app.use(express.json());
@@ -22,24 +40,33 @@ app.set('view engine', 'pug')
 app.post('/addTask', urlencodedParser, (req, res) => {
   if (!req.body) return res.sendStatus(400)
   console.log(req.body)
-  tasksData.push({
-    id: tasksData.length,
+  const user = {
+    id: file.map((a)=>a.openStatus).length,
     priority: req.body.priority,
     info: req.body.info,
     dl: req.body.deadline,
     header: req.body.name,
     worker: req.body.worker,
-    tag: 'Дизайн',
+    tag: req.body.rag,
     color: priorityColor(req.body.priority),
     openStatus: true,
     expandStatus: false,
     progress: 0
+  }
+  jsonfile.readFile(filePath, (err, obj) => {
+    if (err) throw err
+    let fileObj = obj;
+    fileObj.push(user);
+    file.push(user);
+    jsonfile.writeFile(filePath, fileObj, { spaces: 2 }, (err) => {
+      if (err) throw err;
+    })
   })
   res.redirect('/dashboard')
 })
 
 app.get('/dashboard', function (request, response) {
-  response.render('dashboard', dashboard)
+  response.render('dashboard', variables())
 })
 
 app.use('/static', express.static(path.join(__dirname, "img")));
--- db/variables.js
+++ db/variables.js
@@ -1,45 +1,7 @@
-const tasksData = [
-    {
-      id: 0,
-      priority: 'Высокий приоритет',
-      info: `Создать дизайн сайта в фигме, с анимациями, переходами на другие страницы. Каждую страницу размещать по горизонтали. Цвета преимущественно - серый, чёрный и белый. Шрифт - comfortaa`,
-      dl: '28.05.2022 15:00',
-      header: 'Создать дизайн сайта',
-      worker: 'Дмитрий Фролов',
-      tag: 'Дизайн',
-      color: 'red',
-      openStatus: true,
-      expandStatus: false,
-      progress: 60,
-    },
-    {
-      id: 1,
-      priority: 'Высокий приоритет',
-      info: `Сверстать макет по созданному дизайну. Использовать HTML, CSS и JS, без использования сторонних фреймворков`,
-      dl: '14.06.2022 15:00',
-      header: 'Написать код для сайта',
-      worker: 'Николай ашапатов',
-      tag: 'Код',
-      color: 'red',
-      openStatus: true,
-      expandStatus: false,
-      progress: 40,
-    },
-    {
-      id: 2,
-      priority: 'Низкий приоритет',
-      info: `Добавить разнообразный контент по теме оптики. Контентом могут быть статьи, блоги, товары, рецензии и т.д.`,
-      dl: '28.05.2023 15:00',
-      header: 'Добавить контент на главную страницу',
-      worker: 'Тарасов Максим',
-      tag: 'Прочее',
-      color: 'green',
-      openStatus: true,
-      expandStatus: false,
-      progress: 30,
-    },
-  ]
-
+const jsonfile = require("jsonfile");
+const path = require("path");
+const tasksData = jsonfile.readFileSync(path.join(__dirname, "/tasks.json"))
+   
 let openTasks = []
 let closedTasks = []
 
@@ -63,4 +25,4 @@ const priorityColor = (prio) => {
   else if (prio == 'Средний приоритет') return 'yellow'
 } 
 
-module.exports = {variables, tasksData, priorityColor};
+module.exports = { variables, tasksData, priorityColor };
